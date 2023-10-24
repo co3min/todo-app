bff94221d8cc5ee0242b7fd92a7c4679e9aa1dfa
Cosmin Pavel <pavel1cosmin20@stud.ase.ro>
2023-10-24 13:05:37 +0300

    com

--- Todos/delete.js
+++ Todos/delete.js
@@ -4,8 +4,8 @@ import { renderTodos } from "./renderTodos.js";
 export function deleteToDo(e) {
   e.preventDefault();
   console.log("vrei sa stergi un task");
-
-  console.log(e.target.parentElement.id);
+  let todosClicked = document.getElementById(e.target.parentElement.id);
+  console.log(todosClicked);
 
   renderTodos();
 }
